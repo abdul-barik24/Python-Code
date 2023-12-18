23dea2604f7e28d8bb82fcda54f697603ace93d4
amsbarik <amsbarik24@gmail.com>
2023-12-19 05:18:44 +0600

    abstract class and methods updated

---
+++ abstract_class_methods.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+class food(ABC):
+    @abstractmethod
+    def tasty(self):
+        pass
+    
+    def info(self):
+        print(f'Name: {self.name} \nPrice: {self.price} Tk, \nSize: {self.size} inches!')
+
+#subclass
+class pizza(food):
+    def __init__(self, name, size, price):
+        self.name = name
+        self.size = size
+        self.price = price
+
+    def tasty(self):            #the method is overriden
+        print(f'The {self.name} pizza is very delicious!')
+
+
+p1 = pizza('King', '6', 750)
+
+p1.info()
+p1.tasty()
